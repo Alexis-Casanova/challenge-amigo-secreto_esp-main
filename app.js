4ca1d0eb4165300d12aa21aa6769001a11521453
Alexis Casanova <casanovaalexis36@gmail.com>
2025-07-29 16:39:42 -0500

    Creacion de la funcion para agregar

--- app.js
+++ app.js
@@ -2,3 +2,23 @@
 
 // Creamos un arreglo vacío donde se guardarán los nombres ingresados
 let listaDeAmigos = [];
+
+function agregarAmigo() {
+    // Obtenemos el valor del campo de texto (input)
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim(); // quitamos espacios en blanco al inicio y final
+
+    // Validamos si el campo está vacío
+    if (nombre === "") {
+        alert("Por favor, escribe un nombre válido.");
+        return;
+    }
+
+    listaDeAmigos.push(nombre);
+
+    // Mostramos el nuevo nombre en la lista visual en la página
+    mostrarLista(); 
+
+    // Limpiamos el campo de texto para que el usuario pueda escribir otro nombre
+    input.value = "";
+}
