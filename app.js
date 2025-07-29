c07bb5a3cebb08a5cbb8152c10af773f0bd76505
Alexis Casanova <casanovaalexis36@gmail.com>
2025-07-29 16:37:47 -0500

    Creando la lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Creamos un arreglo vacío donde se guardarán los nombres ingresados
+let listaDeAmigos = [];
